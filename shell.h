db329374b83c44fa6956a3437dc9997eec2ccfff
Iadolphe007 <iadolphe007@gmail.com>
2023-05-17 06:30:46 +0000

    header file

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_FILE
+#define SHELL_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+
+
+#endif
