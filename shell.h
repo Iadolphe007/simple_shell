4b5e3f935b874cf1372a568fc62aedd771658db3
bonheurdev <irbonheur3@gmail.com>
2023-05-24 10:49:33 -0700

    prototypes

--- shell.h
+++ shell.h
@@ -28,9 +28,22 @@ void tokenize_command(char *command, char **argv, int max_args);
 void execute_command(char *command_path, char **argv);
 void execute_env_command(char **environ);
 void comd(void);
+void clear_screen(void);
 void change_directory(char *path);
 void handle_variable_replacement(char *command, int status);
 int builtin_echo(char **args);
 int exc_command(char *comd);
+int my_strlen(const char *str);
+char *my_strcpy(char *dest, char *src);
+int my_strncmp(const char *s1, const char *s2, size_t n);
+char *user_getenv(const char *name);
+int execute_exit_command(char *argv[]);
+int execute_cd_command(char *argv[]);
+int execute_echo_command(char *argv[]);
+int execute_other_command(char *argv[]);
+char *get_command_path(char *command);
+char *get_absolute_command_path(char *command);
+char *get_relative_command_path(char *command);
+
 
 #endif
